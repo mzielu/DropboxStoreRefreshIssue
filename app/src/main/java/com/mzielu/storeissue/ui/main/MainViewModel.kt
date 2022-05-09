c3ab26fc3f7285a8982c56e8d1850e5f0c4ea3a5
mzielu <kmarcinzielinski@gmail.com>
2022-05-09 23:17:47 +0200

    Add detailed README description

--- app/src/main/java/com/mzielu/storeissue/ui/main/MainViewModel.kt
+++ app/src/main/java/com/mzielu/storeissue/ui/main/MainViewModel.kt
@@ -2,11 +2,10 @@ package com.mzielu.storeissue.ui.main
 
 import android.app.Application
 import androidx.lifecycle.viewModelScope
-import com.dropbox.android.external.store4.StoreRequest
-import com.dropbox.android.external.store4.fresh
+import com.dropbox.android.external.store4.*
 import com.mzielu.storeissue.MyTestApp
 import com.mzielu.storeissue.ui.base.BaseAndroidViewModel
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -14,11 +13,23 @@ class MainViewModel(app: Application) : BaseAndroidViewModel(app) {
 
     private val store = (app as MyTestApp).roomStore
 
+    val refreshCounter = MutableStateFlow(0)
+    val collectedEventsCounter = MutableStateFlow(0)
+    val state = MutableStateFlow("Init")
+
     init {
         Timber.d("Start observing for events")
         viewModelScope.launch {
-            store.stream(StoreRequest.cached(key = TEST_KEY, refresh = true)).collect {
-                Timber.d("New event from stream: $it")
+            store.stream(StoreRequest.cached(key = TEST_KEY, refresh = true)).collect { storeResponse ->
+                Timber.d("New event from stream: $storeResponse")
+                collectedEventsCounter.update { it + 1 }
+                state.value = when (storeResponse) {
+                    is StoreResponse.Data -> "Data loaded successfully"
+                    is StoreResponse.Loading -> "Loading..."
+                    is StoreResponse.NoNewData -> "No new data loaded"
+                    is StoreResponse.Error.Exception -> "Error with exception"
+                    is StoreResponse.Error.Message -> "Error with message"
+                }
             }
         }
     }
@@ -26,6 +37,8 @@ class MainViewModel(app: Application) : BaseAndroidViewModel(app) {
     fun refresh() {
         Timber.d("Refresh button clicked")
         viewModelScope.launch {
+            refreshCounter.update { it + 1 }
+            state.value = "Refreshing..."
             try {
                 store.fresh(TEST_KEY)
             } catch (e: Throwable) {
